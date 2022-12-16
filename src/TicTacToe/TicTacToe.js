a08669dfb4a5586262fbda5ee572ba4d87bbd2e8
Mielyn Acosta <mielynacosta@pursuit.org>
2022-12-15 23:04:41 -0600

    recogizing which cell box is selecting

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -1,34 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './TicTacToe.css'
 
 const TicTacToe = () => {
- const Cell = () => {
-    return ( <td>-</td>)
+const [turns, setTurns] = useState('x')
+
+const handleClick = () => {
+    if (turns === 'x') {
+        setTurns('o');
+    } else {
+        setTurns('x');
+    }
+    
+}
+
+ const Cell = ({box}) => {
+    return  <td onClick={()=> handleClick(box)}>-</td>
  }
 
 
 
   return (
     <div className="container">
-        <table>
-        <tbody>
-            <tr>
-             <Cell/>
-             <Cell/>
-             <Cell/>
-            </tr>
-            <tr>
-             <Cell/>
-             <Cell/>
-             <Cell/>
-            </tr>
-            <tr>
-             <Cell/>
-             <Cell/>
-             <Cell/>
-            </tr>
-        </tbody>
-        </table>
+      <table>
+				Turn: {turns}
+				<tbody>
+					<tr>
+						<Cell box={0} />
+						<Cell box={1} />
+						<Cell box={2} />
+					</tr>
+					<tr>
+						<Cell box={3} />
+						<Cell box={4} />
+						<Cell box={5} />
+					</tr>
+					<tr>
+						<Cell box={6} />
+						<Cell box={7} />
+						<Cell box={8} />
+					</tr>
+				</tbody>
+			</table>
     </div>
   )
 }
