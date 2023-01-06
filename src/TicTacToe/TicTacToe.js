fbe2acfac9363397e4d08048ecc381793f695d07
Mielyn De Lara-Acosta <43800466+MielynA@users.noreply.github.com>
2023-01-06 09:29:31 -0600

    Added new functionality for winning combos and cells definition  (#2)
    * new branch
    
    * initial commit
    
    * initial approach design:
    
    * recogizing which cell box is selecting
    
    * populating the data in the cells squares
    
    * added some winning combination function

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -1,34 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './TicTacToe.css'
 
 const TicTacToe = () => {
- const Cell = () => {
-    return ( <td>-</td>)
- }
+const [turns, setTurns] = useState('x')
+const [cells, setCells] = useState(Array(9).fill(''))
 
+const winningCombination = (squares) => {
+   let combos = { 
+    across: [[0,1,2],[3,4,5],[6,7,8]],
+	diagonal: [[0,4,8],[2,4,6]],
+	down: [[0,3,6],[1,4,7], [2,5,8]],
+
+   }
+}
+
+const handleClick = (box) => {
+	let squares = [...cells]
+    if (turns === 'x') {
+		squares[box] = 'x'
+        setTurns('o');
+    } else {
+		squares[box] = 'o'
+        setTurns('x');
+    }
+	setCells(squares)
+    console.log(squares)
+}
+
+ const Cell = ({box}) => {
+    return  <td onClick={()=> handleClick(box)}>{cells[box]}</td>
 
 
   return (
     <div className="container">
-        <table>
-        <tbody>
-            <tr>
-             <Cell/>
-             <Cell/>
-             <Cell/>
-            </tr>
-            <tr>
-             <Cell/>
-             <Cell/>
-             <Cell/>
-            </tr>
-            <tr>
-             <Cell/>
-             <Cell/>
-             <Cell/>
-            </tr>
-        </tbody>
-        </table>
+      <table>
+				Turn: {turns}
+				<tbody>
+					<tr>
+						<Cell box={0} />
+						<Cell box={1} />
+						<Cell box={2} />
+					</tr>
+					<tr>
+						<Cell box={3} />
+						<Cell box={4} />
+						<Cell box={5} />
+					</tr>
+					<tr>
+						<Cell box={6} />
+						<Cell box={7} />
+						<Cell box={8} />
+					</tr>
+				</tbody>
+			</table>
     </div>
   )
 }
